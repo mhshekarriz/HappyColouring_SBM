cc9d11b2279a3b98faf31d8929c667901da31217
Iman <143052222+mhshekarriz@users.noreply.github.com>
2023-10-04 12:19:53 +0000

    Pending changes exported from your codespace

--- generate_examples.py
+++ generate_examples.py
@@ -8,6 +8,6 @@ list=[2,3,4,5,6,10]
 for k in list:
     for p in np.arange(0.1,0.9,0.1):
         for q in np.arange(0.001,p/100,0.001):
-            for s in range(10):
+            for s in range(11):
                 genSBM(n,k,p,q,s)
 
