c5085fd54fa130be6a512bd9788986db9cc6a9cd
mhshekarriz <mhshekarriz@gmail.com>
2023-10-22 21:34:29 +1100

    new ways

--- Happy_v.py
+++ Happy_v.py
@@ -2,7 +2,7 @@ import networkx as nx
 import random
 import math
 
-def IsHappy(G,v,r):
+def IsHappy(G,v,r): #checks whether the vertex v is r-happy or not
     i=0
     for t in list(G.adj[v]):
         if (G.nodes[t]["c"]==G.nodes[v]["c"]):
@@ -12,21 +12,21 @@ def IsHappy(G,v,r):
     else:
         return True
 
-def CanBeHappy(G,v,r,k):
+def CanBeHappy(G,v,r,k): #Checks if the vertex v can be r-happy if there are k colours
     i=0
     iu=0
-    if (G.nodes[v]["c"]!="u"):
+    if (G.nodes[v]["c"]!="u"): #if v is not coloured yet
         for t in list(G.adj[v]):
             if (G.nodes[t]["c"]==G.nodes[v]["c"]):
                 i+=1
             if (G.nodes[t]["c"]=="u"):
                 iu+=1
-    else:
+    else: #if v is already coloured
         max_c=[]
         for s in range(k):
                     max_c.append([])
         for t in list(G.adj[v]):
-            if (G.nodes[t]["c"]!="u"]):
+            if (G.nodes[t]["c"]!="u"):
                 max_c[G.nodes[t]["c"]].append(t)
             else:
                 iu+=1
@@ -34,14 +34,14 @@ def CanBeHappy(G,v,r,k):
     return i+iu
 
 
-def Happy_v(G,r):
+def Happy_v(G,r): #gives the list of r-happy vertices in a partially coloured graph G
     Hv=[]
     for v in list(G.nodes):
          if (IsHappy(G,v,r)==True):
              Hv.append(v)
     return Hv
 
-def P_v(G,r,k):
+def P_v(G,r,k): #gives the list of P-vertices
     Pv=[]
     for v in list(G.nodes):
         if (G.nodes[v]["c"]!="u" and IsHappy(G,v,r)==False and CanBeHappy(G,v,r,k)==True):
@@ -53,7 +53,7 @@ def P_v(G,r,k):
 #def L_p(G,r):
 
 
-def L_h(G,r,k):
+def L_h(G,r,k): #gives the list of L_h vertices
     Lh=[]
     for v in list(G.nodes):
         if (G.nodes[v]["c"]=="u" and CanBeHappy(G,v,r,k)==True):
@@ -63,17 +63,17 @@ def L_h(G,r,k):
         
 
 
-def L_u(G,r,k):
+def L_u(G,r,k): #gives the list of L_u vertices
     Lu=[]
     for v in list(G.nodes):
-        if (G.nodes[v]["c"]=="u" and CanBeHappy(G,v,r,k)==Frue):
+        if (G.nodes[v]["c"]=="u" and CanBeHappy(G,v,r,k)==False):
             Lu.append(v)
     return Lu
 
 #def L_f(G,r):
 
 
-def greedy1_HC_r(G,V,U,r):
+def greedy1_HC_r(G,V,U,r): #the greedy algorithm as it is mentioned in Peng Zhang and Angsheng Li. Algorithmic aspects of homophyly of networks. Theoretical Computer Science, 593:117–131, 2015
     Graph=G
     Vc=V
     Uc=U
@@ -135,7 +135,7 @@ def greedy3_HC_r(G,V,U,r):
         for s in range(k):
             max_c.append([])
         for t in list(G.adj[u]):
-            if (G.nodes[t]["c"]!="u"]):
+            if (G.nodes[t]["c"]!="u"):
                 max_c[G.nodes[t]["c"]].append(t)
         i=0
         cq='u'
@@ -144,7 +144,7 @@ def greedy3_HC_r(G,V,U,r):
                 i=len(max_c[q])
                 cq=q
         G.nodes[u]["c"]=cq
-        V[cq].append(u)
+        V[cq].add(u)
     U=set()
     return G,V
                     
@@ -153,8 +153,9 @@ def greedy3_HC_r(G,V,U,r):
 def growth_HC_r(G,V,U,r):
     k=len(V)
     while (U!=set()):
-        while (P_v(G,r,k)!=[]):
-            v=random.choice(P_v(G,r,k))
+        A=P_v(G,r,k)
+        while (A!=[]):
+            v=random.choice(A)
             Up=list(G.adj(v)).intersection(U)
             i=0
             for u in list(G.adj(v)):
@@ -167,9 +168,10 @@ def growth_HC_r(G,V,U,r):
                 Up.remove(x)
                 U.remove(x)
                 V[G.nodes[v]["c"]].append(x)
-
-        while (P_v(G,r,k)==[] and L_h(G,r,k)!=[]):
-             v=random.choice(L_h(G,r,k))
+            A=P_v(G,r,k)
+        B=L_h(G,r,k)
+        while (A==[] and B!=[]):
+            v=random.choice(B)
             Up=list(G.adj(v)).intersection(U)
             i=0
             for u in list(G.adj(v)):
@@ -179,7 +181,7 @@ def growth_HC_r(G,V,U,r):
             for s in range(k):
                 max_c.append([])
             for w in list(G.adj[v]):
-                if (G.nodes[w]["c"]!="u"]):
+                if (G.nodes[w]["c"]!="u"):
                     max_c[G.nodes[w]["c"]].append(t)
             t=math.ceil(r*G.deg(v))-i
             l=0
@@ -196,9 +198,11 @@ def growth_HC_r(G,V,U,r):
                 Up.remove(x)
                 U.remove(x)
                 V[cq].append(x)
-
-        while (P_v(G,r,k)==[] and L_h(G,r,k)==[] and L_u(G,r,k)!=[]):
-            v=random.choice(L_u(G,r,k))
+            A=P_v(G,r,k)
+            B=L_h(G,r,k)
+        C=L_u(G,r,k)
+        while (A==[] and B==[] and C!=[]):
+            v=random.choice(C)
             Up=list(G.adj(v)).intersection(U)
             i=0
             for u in list(G.adj(v)):
@@ -208,7 +212,7 @@ def growth_HC_r(G,V,U,r):
             for s in range(k):
                 max_c.append([])
             for w in list(G.adj[v]):
-                if (G.nodes[w]["c"]!="u"]):
+                if (G.nodes[w]["c"]!="u"):
                     max_c[G.nodes[w]["c"]].append(t)
             t=math.ceil(r*G.deg(v))-i
             l=0
@@ -225,5 +229,7 @@ def growth_HC_r(G,V,U,r):
                 Up.remove(x)
                 U.remove(x)
                 V[cq].append(x)
-            
+            A=P_v(G,r,k)
+            B=L_h(G,r,k)
+            C=L_u(G,r,k)
     return G,V
--- generate_examples.py
+++ generate_examples.py
@@ -2,10 +2,10 @@ from genSBM import *
 import numpy as np
 
 n=1200
-list=[2,3,4,5,6,10]
+parts=[2,3,4,5,6,10]
 
 
-for k in list:
+for k in parts:
     for p in np.arange(0.1,0.9,0.1):
         for q in np.arange(0.001,p/100,0.001):
             for s in range(11):
