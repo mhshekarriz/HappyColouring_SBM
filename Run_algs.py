fefa8522c28fff54c823df0d83acbfd2e749edad
Iman <143052222+mhshekarriz@users.noreply.github.com>
2023-10-29 21:25:54 +1100

    Update Run_algs.py

--- Run_algs.py
+++ Run_algs.py
@@ -24,21 +24,21 @@ for k in parts:
                 comment1="c n="+str(n)+" k="+str(k)+" p="+str(round(p,2))+" q="+str(round(q,3))+" seed="+str(seed)+"\n"
                 write_dimacs(G,V,path_inp+filename+".txt",comment1)
                 for r in np.arange(0.1,1.1,0.1):
-                    [F,T]=greedy3_HC_r(G,V,U,r)
+                    [F,T,ctgreedy]=greedy3_HC_r(G,V,U,r)
                     Av=[]
                     for t in range(len(T)):
                         Av.append(len(T[t]))
-                    comment2=comment1+"c Algorithm: greedy3_HC_r \n"+"c r="+str(r)+"\n"+"c The number happy vertices: "+str(len(Happy_v(F,r)))+"\n"+"c Fraction of happy vertices: "+str(round(len(Happy_v(F,r))/n,4))+"\n"+"c Vertex partition sizes are "+str(Av)+"\n"
+                    comment2=comment1+"c Algorithm: greedy3_HC_r \n"+"Time consumed: "+str(ctgreedy)+"\n"+"c r="+str(r)+"\n"+"c The number happy vertices: "+str(len(Happy_v(F,r)))+"\n"+"c Fraction of happy vertices: "+str(round(len(Happy_v(F,r))/n,4))+"\n"+"c Vertex partition sizes are "+str(Av)+"\n"
                     write_dimacs(F,T,path_out1+"Out_"+filename+"_r="+str(round(r,3))+".txt",comment2)
                     i+=1
                     ratio_greedy+=len(Happy_v(F,r))/n
 
 
-                    [H,S]=growth_HC_r(G,V,U,r)
+                    [H,S,ctgrowth]=growth_HC_r(G,V,U,r)
                     Bv=[]
                     for s in range(len(S)):
                         Bv.append(len(S[s]))
-                    comment3=comment1+"c Algorithm: growth_HC_r \n"+"c r="+str(r)+"\n"+"c The number happy vertices: "+str(len(Happy_v(H,r)))+"\n"+"c Fraction of happy vertices: "+str(round(len(Happy_v(H,r))/n,4))+"\n"+"c Vertex partition sizes are "+str(Bv)+"\n"
+                    comment3=comment1+"c Algorithm: growth_HC_r \n"+"Time consumed: "+str(ctgrowth)+"\n"+"c r="+str(r)+"\n"+"c The number happy vertices: "+str(len(Happy_v(H,r)))+"\n"+"c Fraction of happy vertices: "+str(round(len(Happy_v(H,r))/n,4))+"\n"+"c Vertex partition sizes are "+str(Bv)+"\n"
                     write_dimacs(H,S,path_out2+"Out_"+filename+"_r="+str(round(r,3))+".txt",comment3)
                     ratio_growth+=len(Happy_v(H,r))/n
 
